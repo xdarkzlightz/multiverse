c12408491da6f35a8b004ca3494a881b735c12a2
xdarkzlightz <xdarkzdev@gmail.com>
2019-07-07 16:35:19 +0000

    Replaced container buttons with clickable icons

--- src/client/src/components/Container.js
+++ src/client/src/components/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import ContainerButton from "./ContainerButton";
 
 export default ({ name, id, running, port, setContainers }) => {
   const doAction = async action => {
@@ -11,38 +11,51 @@ export default ({ name, id, running, port, setContainers }) => {
     setContainers(res.data.containers);
   };
 
-  const cardStyle = { width: "16rem" };
+  const cardStyle = { width: "14rem" };
 
   return (
     <Card style={cardStyle} bg="secondary" text="light">
       <Card.Header className="text-center">
         {name.replace(/_/g, " ")}
       </Card.Header>
-      <Card.Footer className="d-flex justify-content-around">
+      <Card.Footer>
         {running ? (
-          <>
-            <Button
-              variant="success"
+          <div className="d-flex">
+            <ContainerButton
+              variant="open"
               onClick={() =>
                 window.open(`http://${window.location.hostname}:${port}`)
               }
-            >
-              Open
-            </Button>
-            <Button onClick={() => doAction("stop")} variant="danger">
-              Stop
-            </Button>
-            <Button onClick={() => doAction("kill")} variant="danger">
-              Kill
-            </Button>
-          </>
+            />
+
+            <div className="ml-auto">
+              <span className="mr-2">
+                <ContainerButton
+                  variant="stop"
+                  onClick={() => doAction("stop")}
+                />
+              </span>
+
+              <ContainerButton
+                variant="kill"
+                onClick={() => doAction("kill")}
+              />
+            </div>
+          </div>
         ) : (
-          <>
-            <Button variant="success">Start</Button>
-            <Button onClick={() => doAction("remove")} variant="danger">
-              Remove
-            </Button>
-          </>
+          <div class="d-flex">
+            <ContainerButton
+              variant="start"
+              onClick={() => doAction("start")}
+            />
+            <span className="ml-auto">
+              <ContainerButton
+                variant="remove"
+                onClick={() => doAction("remove")}
+                className="ml-5"
+              />
+            </span>
+          </div>
         )}
       </Card.Footer>
     </Card>
---
+++ src/client/src/components/ContainerButton.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faExternalLinkAlt,
+  faStopCircle,
+  faBan,
+  faPlayCircle,
+  faTrashAlt
+} from "@fortawesome/free-solid-svg-icons";
+
+export default ({ variant, onClick }) => {
+  let icon;
+  let color;
+  if (variant === "open") {
+    icon = faExternalLinkAlt;
+    onClick = color = "green";
+  } else if (variant === "start") {
+    icon = faPlayCircle;
+    color = "green";
+  } else if (variant === "stop") {
+    icon = faStopCircle;
+    color = "red";
+  } else if (variant === "kill") {
+    icon = faBan;
+    color = "red";
+  } else if (variant === "remove") {
+    icon = faTrashAlt;
+    color = "red";
+  }
+
+  return (
+    <span>
+      <style>
+        {`
+                .icon:hover {
+                    cursor: pointer;
+                }
+            `}
+      </style>
+      <OverlayTrigger
+        key="top"
+        placement="top"
+        overlay={<Tooltip id={`tooltip-top`}>{variant}</Tooltip>}
+      >
+        <FontAwesomeIcon
+          icon={icon}
+          onClick={onClick}
+          style={{ color }}
+          size="lg"
+          className="icon"
+        />
+      </OverlayTrigger>
+    </span>
+  );
+};
