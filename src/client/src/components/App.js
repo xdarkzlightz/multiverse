b36fae0836e9405fe41bb5e0a620a3f51910838e
xdarkzlightz <xdarkzdev@gmail.com>
2019-07-08 05:18:16 +0000

    Moved container components to their own folder

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Containers from "./Containers";
+import Containers from "./containers/Containers";
 import Header from "./Header";
-import InstanceCreator from "./InstanceCreator";
+import InstanceCreator from "./instanceForm/InstanceCreator";
 
 export default () => {
   return (
