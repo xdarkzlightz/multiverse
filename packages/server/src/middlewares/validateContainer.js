7a0f427a4706c28f53a1438ca9a312d418d327d3
xdarkzlightz <xdarkzdev@gmail.com>
2019-07-16 05:42:03 +0000

    Container name validation

---
+++ packages/server/src/middlewares/validateContainer.js
@@ -0,0 +1,17 @@
+const Docker = require('../services/docker')
+
+const docker = new Docker()
+
+module.exports = async (req, res, next) => {
+  const containers = await docker.getContainers()
+  const { name } = req.body
+
+  const nameInUse = containers.some(
+    c => c.Labels['multiverse.project'] === name
+  )
+  if (nameInUse) {
+    return res.status(400).send(`Name ${name} is already in use`)
+  }
+
+  next()
+}
