a103f8aff90907d4947f7247a30b6779536ccb67
xdarkzlightz <xdarkzdev@gmail.com>
2019-07-18 05:59:46 +0000

    Fixed friendly error handling

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -4,6 +4,7 @@ const logger = require('koa-morgan')
 const join = require('path').join
 const dockerRoutes = require('./routes/container-routes')
 const winston = require('./utils/winston')
+const FriendlyError = require('./errors/FriendlyError')
 
 const app = new Koa()
 const env = process.env.NODE_ENV
@@ -11,6 +12,22 @@ const env = process.env.NODE_ENV
 app.use(bodyParser())
 // Don't want to be logging endpoints if in a test environment
 if (env !== 'test') app.use(logger('combined', { stream: winston.stream }))
+
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    if (err instanceof FriendlyError) {
+      ctx.status = 400
+      ctx.body = err.message
+      ctx.app.emit('error', err, ctx)
+    } else {
+      ctx.status = err.status || 500
+      ctx.body = err.message
+      ctx.app.emit('error', err, ctx)
+    }
+  }
+})
 
 app.use(dockerRoutes.routes())
 // Multiverse-Server can optionally serve a client using the GET / endpoint
--- packages/server/src/errors/FriendlyError.js
+++ packages/server/src/errors/FriendlyError.js
@@ -2,6 +2,8 @@ module.exports = class FriendlyError extends Error {
   constructor (message, data) {
     super(data)
 
+    this.status = 400
+
     if (Array.isArray(message)) {
       let msg = ''
       message.forEach(_msg => (msg += _msg))
