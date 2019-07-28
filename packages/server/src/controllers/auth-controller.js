e41d84de61f2cd1367853c7d1d89366d977ba253
xdarkzlightz <xdarkzdev@gmail.com>
2019-07-28 05:22:58 +0000

    Updates the user's firstLogin property

--- packages/server/src/controllers/auth-controller.js
+++ packages/server/src/controllers/auth-controller.js
@@ -1,15 +1,20 @@
 const passport = require('koa-passport')
 const jwt = require('jsonwebtoken')
+const UserService = require('../services/UserService')
 
+const userService = new UserService()
 module.exports.login = async (ctx, next) => {
   return passport.authenticate(
     'local',
     { session: false },
-    (err, user, info) => {
+    async (err, user, info) => {
       if (err) console.log(err)
       if (user) {
         // The secret key will be provided by a config provider once authentication and accounts are finished
         const token = jwt.sign({ id: user.id }, 'secureLater')
+        if (user.firstLogin) {
+          await userService.updateUser(user.id, { firstLogin: false })
+        }
         ctx.status = 200
         ctx.body = { success: true, token }
       } else {
