516a0dfed0d3f5eae1f9df16413adc4cc6fc7d90
xdarkzlightz <xdarkzdev@gmail.com>
2019-07-30 19:29:17 +0000

    Added a list view component

--- packages/client/src/components/projects/ProjectsList.js
+++ packages/client/src/components/projects/ProjectsList.js
@@ -1,130 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { Redirect } from "react-router-dom";
+import React from "react";
 
-import Project from "./Project";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import ProjectsListItem from "./ProjectListItem";
 
-import Header from "../Header";
-import Toolbar from "./ProjectsToolbar";
-import { UserConsumer } from "../../context/UserContext";
-
-import useApi from "../../hooks/useApi";
-
-const sort = (data, type) => {
-  let filter;
-  if (type === "a-z") {
-    filter = (a, b) => {
-      if (a.name > b.name) return 1;
-      if (a.name < b.name) return -1;
-      return 0;
-    };
-  } else if (type === "z-a") {
-    filter = (a, b) => {
-      if (a.name < b.name) return 1;
-      if (a.name > b.name) return -1;
-      return 0;
-    };
-  } else if (type === "created by: a-z") {
-    filter = (a, b) => {
-      if (a.username > b.username) return 1;
-      if (a.username < b.username) return -1;
-      return 0;
-    };
-  } else if (type === "created by: z-a") {
-    filter = (a, b) => {
-      if (a.username < b.username) return 1;
-      if (a.username > b.username) return -1;
-      return 0;
-    };
-  } else if (type === "created at: oldest") {
-    filter = (a, b) => {
-      const aCreatedAt = new Date(a.createdAt);
-      const bCreatedAt = new Date(b.createdAt);
-
-      if (aCreatedAt > bCreatedAt) return 1;
-      if (aCreatedAt < bCreatedAt) return -1;
-      return 0;
-    };
-  } else if (type === "created at: newest") {
-    filter = (a, b) => {
-      const aCreatedAt = new Date(a.createdAt);
-      const bCreatedAt = new Date(b.createdAt);
-
-      if (aCreatedAt < bCreatedAt) return 1;
-      if (aCreatedAt > bCreatedAt) return -1;
-      return 0;
-    };
-  }
-
-  data.sort(filter);
-};
-
-const filterData = (data, value, type) => {
-  let filter;
-  if (["a-z", "z-a"].includes(type)) {
-    filter = c => c.name.includes(value);
-  } else if (["created by: a-z", "created by: z-a"].includes(type)) {
-    filter = c => c.username.includes(value);
-  } else if (["created at: oldest", "created at: newest"].includes(type)) {
-    filter = c => {
-      const createdAt = new Date(c.createdAt);
-      return createdAt.toDateString().includes(value);
-    };
-  }
-
-  return data.filter(filter);
-};
-
-export default () => {
-  const [{ data, loading, error, auth }, fetchData] = useApi([]);
-  const [filter, setFilter] = useState("a-z");
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    fetchData("/api/containers");
-  }, [fetchData]);
-
-  sort(data, filter);
-
-  const containers = filterData(data, search, filter);
-
-  if (loading) {
-    return <></>;
-  } else if (error && !auth) {
-    return <Redirect to="/login" />;
-  } else if (error) {
-    return <h1>Error has occured</h1>;
-  } else {
-    return (
-      <>
-        <UserConsumer>{data => <Header {...data} />}</UserConsumer>
-        <Container>
-          <h1 className="text-center display-4 mb-4">Projects</h1>
-          <Toolbar
-            setFilter={setFilter}
-            filter={filter}
-            search={search}
-            setSearch={setSearch}
-          />
-          <Row className="mt-3">
-            {containers.map(c => (
-              <Col
-                key={c.id}
-                xs={12}
-                sm={6}
-                md={6}
-                lg={4}
-                xl={3}
-                className="mb-3"
-              >
-                <Project data={c} setContainers={fetchData} />
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </>
-    );
-  }
+export default ({ containers }) => {
+  return (
+    <>
+      <Table className="mt-3">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Created by</th>
+            <th>Created at</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {containers.map(c => (
+            <ProjectsListItem key={c.id} {...c} />
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
 };
