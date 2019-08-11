790ad4402671724f5d9d3a2b4b226e8cd47e804a
xdarkzlightz <xdarkzdev@gmail.com>
2019-08-11 00:21:06 +0000

    Moved routes to it's own file

--- packages/client/src/components/App.js
+++ packages/client/src/components/App.js
@@ -1,68 +1,7 @@
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
+import React from "react";
 
-import axios from "axios";
-
-import ProjectsPage from "./ProjectsPage";
-import InstanceCreator from "./projectForm/ProjectForm";
-import Login from "./LoginPage";
-import UsersPage from "./UsersPage";
-
-import UserContext from "../context/UserContext";
-import UsersForm from "./userForm/UsersForm";
-import ContextRoute from "./ContextRoute";
+import Routes from "./routes/Routes";
 
 export default () => {
-  const [user, setUser] = useState({});
-  const token = localStorage.getItem("token");
-  const id = localStorage.getItem("user_id");
-
-  if (token && id && !user.id) {
-    axios
-      .get(`/api/users/${id}`, {
-        headers: { Authorization: `Bearer ${token}` }
-      })
-      .then(res => setUser(res.data))
-      .catch(e => console.log(e));
-  }
-
-  return (
-    <Router>
-      <Switch>
-        <ContextRoute
-          path="/projects"
-          component={ProjectsPage}
-          contextComponent={UserContext}
-          initValue={{ user, setUser }}
-        />
-        <ContextRoute
-          path="/users"
-          component={UsersPage}
-          contextComponent={UserContext}
-          initValue={{ user, setUser }}
-        />
-        <ContextRoute
-          path="/create/project"
-          component={InstanceCreator}
-          contextComponent={UserContext}
-          initValue={{ user, setUser }}
-        />
-        <ContextRoute
-          path="/create/user"
-          component={UsersForm}
-          contextComponent={UserContext}
-          initValue={{ user, setUser }}
-        />
-        <Route path="/login">
-          <Login setUser={setUser} user={user} />
-        </Route>
-        <Redirect from="/" exact to="/projects" />
-      </Switch>
-    </Router>
-  );
+  return <Routes />;
 };
